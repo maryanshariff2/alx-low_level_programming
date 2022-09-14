9b0bde11f9d504c704fd2c8f37b183a4abcf870b
Maryan shariff <maryanshariff2@gmail.com>
2022-09-14 10:46:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by new line
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('r');
+
+	return (0);
+}
