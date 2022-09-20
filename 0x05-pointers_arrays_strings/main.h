68420641978a5a3fc99ed68c3206e8b176a3749b
Maryan shariff <maryanshariff2@gmail.com>
2022-09-20 05:14:39 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
